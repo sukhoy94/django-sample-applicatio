7586ff6625385b416a91c70cfa3a7297d3839c97
andrii <andrii.sukhoi@nocowanie.pl>
2021-02-08 22:21:34 +0100

    django templates

--- news/views.py
+++ news/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World')
+    news = News.objects.all();
+    return render(request, 'news/index.html', {'news': news, 'title': 'News List'})
 
 
 def test(request):
